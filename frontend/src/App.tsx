d3470327bde1b244fd3b65bbdaf9b70b9d6dbfea
meetshukla <meet_shukla@trendmicro.com>
2024-11-27 15:26:48 -0500

    fix content center

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import FileUpload from "./components/FileUpload";
-import Chat from "./components/Chat";
+import FileUpload from './components/FileUpload';
+import Chat from './components/Chat';
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="min-h-screen bg-gray-50 py-8 w-full space-y-4 w-full flex flex-col justify-center items-center">
       <FileUpload />
       <Chat />
     </div>
